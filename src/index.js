9ea750c9dca77f33525b4f561f1cc2898fc7a670
Alfredo Sumaran <a.sumaran@gmail.com>
2017-07-26 06:48:47 -0500

    First commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <h1>Hello world from React.js</h1>,
+    document.getElementById('root')
+  );
+});
